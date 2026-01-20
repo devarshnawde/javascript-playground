011329048f30a50ce7fd382c04c3d66b2bf7dab1
its-devarsh <dnawde@intelligentdx.com>
2026-01-20 12:18:10 +0530

    feat: Add array sum example using the reduce method.

---
+++ basics/arraySum.js
@@ -0,0 +1,5 @@
+// Sum all elements in an array
+const numbers = [1, 2, 3, 4, 5];
+const sum = numbers.reduce((acc, cur) => acc + cur, 0);
+console.log('Array:', numbers);
+console.log('Sum:', sum);
